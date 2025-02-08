800a55c51381ed4f36eb1f48d5dec5bdccb9af2f
Luis Alvarez <mrluisfeer@gmail.com>
2025-02-07 18:08:44 -0600

    remove sensitive variables

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,5 @@ export const API_TOKEN_URL = "https://accounts.spotify.com/api/token";
 
 export const SPOTIFY_ARTIST_ID = "2jku7tDXc6XoB6MO2hFuqg";
 
-export const CLIENT_ID =
-  process.env.SPOTIFY_CLIENT_ID || "330524c072e14717ab5e29e8b417c42d";
-export const CLIENT_SECRET =
-  process.env.SPOTIFY_CLIENT_SECRET || "d4e3a238109a4ed18d1a20d382ce0a1c";
+export const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+export const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
